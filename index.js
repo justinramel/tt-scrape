9dc52375e85e53b4c4d29e5c2bb46bfcdb759b28
Justin Ramel <justin.ramel@gmail.com>
2017-04-18 21:42:46 +0100

    output comma delimited list of previous results

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 // https://www.digitalocean.com/community/tutorials/how-to-use-node-js-request-and-cheerio-to-set-up-simple-web-scraping
+const request = require('request')
+const cheerio = require('cheerio')
 
-var request = require('request')
-var cheerio = require('cheerio')
+const eventUrl = 'https://www.cyclingtimetrials.org.uk/race-entrant-list/14861'
+const course = 'M1010B'
+const distance = '10 Miles'
 
-request('https://www.cyclingtimetrials.org.uk/race-entrant-list/14861#anchor', function (error, response, html) {
+request(eventUrl, function (error, response, html) {
   if (!error && response.statusCode === 200) {
-    var $ = cheerio.load(html)
-    var headers = $('#racesUsersTable thead tr th').map((index, header) => header.children[0].data)
-    debugger
-    console.log(headers)
+    let riders = []
+    let $ = cheerio.load(html)
+    // let headers = $('#racesUsersTable thead tr th').map((index, header) => $(header).text()).get()
+    // headers.push('Results Url')
+    // riders.push(headers)
+    $('#racesUsersTable tbody tr').each((index, tr) => {
+      let data = $(tr).find('td').map((index, td) => $(td).text()).get()
+      let riderId = $(tr).find('a').attr('href').split('/')[2]
+      data.push(riderId)
+      riders.push(data)
+    })
+    fetchResults(riders)
   }
 })
+
+function fetchResults (riders) {
+  riders.forEach(rider => {
+    const riderId = rider[7]
+    request(`https://www.cyclingtimetrials.org.uk/rider-results/${riderId}`, function (error, response, html) {
+      let results = []
+
+      if (!error && response.statusCode === 200) {
+        let $ = cheerio.load(html)
+        $('#eventsTable tbody tr').each((index, tr) => {
+          let data = $(tr).find('td').map((index, td) => $(td).text()).get()
+          results.push(data)
+        })
+      }
+      console.log(rider.concat(bestResult(results)).join(','))
+    })
+  })
+}
+
+function bestResult (results) {
+  const courseIndex = 2
+  const distanceIndex = 3
+  const positionIndex = 5
+  let found = results.filter(result => result[courseIndex].toUpperCase() === course)
+                      .filter(result => result[positionIndex].toUpperCase() !== 'DNF' && result[positionIndex].toUpperCase() !== 'DNS')
+
+  if (found.length > 0) {
+    return found[0]
+  }
+
+  found = results.filter(result => result[distanceIndex] === distance)
+                  .filter(result => result[positionIndex].toUpperCase() !== 'DNF' && result[positionIndex].toUpperCase() !== 'DNS')
+
+  if (found.length > 0) {
+    return found[0]
+  } else {
+    return ['', '', '', '', '', '', '', '']
+  }
+}
