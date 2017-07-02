bbf4892b12c1f44495d3c09face36b94de07a497
Justin Ramel <justin.ramel@gmail.com>
2017-07-02 22:08:45 +0100

    scrape m2511

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-const eventUrl = 'https://www.cyclingtimetrials.org.uk/race-entrant-list/15378'
-const course = 'M9'
-const distance = '10 Miles'
+const eventUrl = 'https://www.cyclingtimetrials.org.uk/race-entrant-list/15524'
+const course = 'M2511'
+const distance = '25 Miles'
 
 axios.get(eventUrl)
   .then(extractRiders)
